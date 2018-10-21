725e5704c22b4777673070c16ab5fe1237c58f34
arzieba1420 <arzieba@wszib.edu.pl>
2018-10-21 17:10:37 +0200

    lab33

--- src/Lab3_2_3.java
+++ src/Lab3_2_3.java
@@ -8,6 +8,7 @@ public class Lab3_2_3 {
     public static void main(String[] args) {
 
         Lab3_2_3 l1 = new Lab3_2_3();
+
         Lab3_2_3 l2 = new Lab3_2_3();
         Lab3_2_3 l3 = new Lab3_2_3();
         System.out.println(counter);
