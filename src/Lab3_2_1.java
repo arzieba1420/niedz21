7ca48e84adb199f54a3dec2547a4a19ff3c74ad2
arzieba1420 <44318275+arzieba1420@users.noreply.github.com>
2018-10-23 23:44:48 +0200

    TEST w domu

--- src/Lab3_2_1.java
+++ src/Lab3_2_1.java
@@ -4,18 +4,20 @@ public class Lab3_2_1 {
     public static void main(String[] args) {
 
         StringTokenizer s1;
-        String dd,mm,rrrr;
+        String dd, mm, rrrr;
 
         String data = "07/11/1993";
 
-        s1 = new StringTokenizer(data,"/");
+        s1 = new StringTokenizer(data, "/");
 
+        System.out.println(s1.countTokens());
         dd = s1.nextToken();
         mm = s1.nextToken();
         rrrr = s1.nextToken();
 
-        System.out.println("Dzień: "+dd);
-        System.out.println("Miesiąc: "+mm);
-        System.out.println("Rok: "+rrrr);
+        System.out.println("Dzień: " + dd);
+        System.out.println("Miesiąc: " + mm);
+        System.out.println("Rok: " + rrrr);
+
     }
 }
